889f574340901c92661433e3074c966888f2eb25
hsshimde <hsshimde@outlook.com>
2020-11-10 14:26:55 +0900

    Implementation process

--- PortfolioManagement/CompanyFinancialInfo.h
+++ PortfolioManagement/CompanyFinancialInfo.h
@@ -6,12 +6,20 @@ namespace company
 	{
 	public:
 		Company(size_t companyKey, double marketValue, double sales, double operatingProfit, double netProfit);
+		virtual ~Company();
 
 		void SetMarketValue(double marketValue);
 		void SetSales(double Sales);
 		void SetOperatingProfit(double operatingProfit);
 		void SetNetProfit(double netProfit);
 	
+		size_t GetCompanyKey() const;
+		double GetMarketValue() const;
+		double GetSales() const;
+		double GetOperatingProfit() const;
+		double GetNetProfit() const;
+
+
 
 
 	private:
@@ -20,6 +28,7 @@ namespace company
 		double mSales;
 		double mOperatingProfit;
 		double mNetProfit;
+		float mLastYearGrowthRate[12];
 		
 	};
 }
--- PortfolioManagement/Portfolio.h
+++ PortfolioManagement/Portfolio.h
@@ -1,23 +1,46 @@
 #pragma once
 #include <utility>
+#include <vector>
 
 #include "CompanyFinancialInfo.h"
+
+constexpr static size_t MAX_PORTFOLIO_SIZE{ 100 };
 
 namespace portfolio
 {
 	using namespace company;
-	typedef size_t stockCount;
-	typedef std::pair<Company, stockCount>  
-	constexpr size_t PORTFOLIO_STOCK_COUNT;
+	typedef float weight;
+	typedef std::pair<Company, weight> IndividualStock;
+	typedef std::vector<IndividualStock*> StockVector;
+	
 	class Portfolio
 	{
+		friend class company::Company;
+
 	public:
+		Portfolio();
+		virtual ~Portfolio();
+		Portfolio(const Portfolio& rhs);
+		Portfolio& operator=(const Portfolio& rhs);
+		
+		StockVector SortByShare() const;
+		StockVector SortByPBR() const;
+		StockVector SortByPCR() const;
+		StockVector SortByPOR() const;
+		
+		StockVector SortByGeometricMean() const;
+		StockVector SortByArithmeticMean() const;
+		
+			
+	
+	private:
+		const IndividualStock* mStockArray[MAX_PORTFOLIO_SIZE];
+		size_t mPortfolioSize;
 		
 
-	private:
-		Company* mStockArray;
-		size_t mStockCount;
-		double mPortfolioNetValue;
-
 	};
+
+
+	
+
 }
