dc455a33472a5859d01b4fcd769139e05c92e42c
hsshimde <hsshimde@outlook.com>
2020-11-11 14:37:28 +0900

    implementation process

--- MyOpenAddressHashTable/HashMap.cpp
+++ MyOpenAddressHashTable/HashMap.cpp
@@ -1,14 +1,12 @@
+#include <new>
 #include "HashMap.h"
 
 namespace hashmap
 {
-	/*eState* mStateArray;
-	std::pair<K, V>* mData;
-	size_t mCapacity;
-	size_t mSize;*/
+
 	template <typename K, typename V>
 	HashMap<K, V>::HashMap(size_t size)
-		: mCapacity{size}
+		: mCapacity{ size }
 		, mSize{}
 	{
 		mStateArray = new eState[size];
@@ -22,19 +20,15 @@ namespace hashmap
 
 	template <typename K, typename V>
 	HashMap<K, V>::HashMap(const HashMap& rhs)
-		: mCapacity{rhs.mCapacity}
-		, mSize{rhs.mSize}
+		: mCapacity{ rhs.mCapacity }
+		, mSize{ rhs.mSize }
 	{
 		mStateArray = new eState[mCapacity];
 		mData = new std::pair<K, V>[mSize];
 
 		for (size_t i{}; i < mCapacity; ++i)
 		{
-			if (i < mSize)
-			{
-				mData[i] = rhs.mData[i];
-			}
-
+			mData[i] = rhs.mData[i];
 			mStateArray[i] = rhs.mStateArray[i];
 		}
 
@@ -54,12 +48,61 @@ namespace hashmap
 
 		for (size_t i{}; i < mCapacity; ++i)
 		{
-			if (i < mSize)
-			{
-				mData[i] = rhs.mData[i];
-			}
-			
+			mData[i] = rhs.mData[i];
 			mStateArray[i] = rhs.mStateArray[i];
 		}
+	}
+
+	template <typename K, typename V>
+	HashMap<K, V>::~HashMap()
+	{
+		delete[] mData;
+		delete[] mStateArray;
+	}
+
+	template <typename K, typename V>
+	void HashMap<K, V>::Resize(size_t size)
+	{
+		eState* newStateArray = new eState[size];
+		std::pair<K, V>* newDataArray = new std::pair<K, V>[size];
+
+		for (size_t i{}; i < )
+	}
+
+	template <typename K, typename V>
+	bool HashMap<K, V>::Insert(const std::pair<K, V>& element)
+	{
+		if (static_cast<float>(mSize + 1) / mCapacity >= mLoadFactor)
+		{
+			reHash();
+		}
+
+		size_t hashTryTimes{};
+		K insertElementKeyValue{ element.first };
+
+		size_t hashValue{ getHashValue(insertElementKeyValue, hashTryTimes) };
+
+
+		while (mStateArray[hashValue] == eState::Used)
+		{
+			if (mData[hashValue].first == insertElementKeyValue)
+			{
+				return false;
+			}
+
+			hashTryTimes++;
+			hashValue = getHashValue(insertElementKeyValue, hashTryTimes);
+		}
+
+		mStateArray[hashValue] = eState::Used;
+		mData[hashValue] = element;
+
+		return true;
+	}
+
+	template <typename K, typename V>
+	bool HashMap<K, V>::Remove(const K& keyValue)
+	{
+		
 	}
 }
--- MyOpenAddressHashTable/HashMap.h
+++ MyOpenAddressHashTable/HashMap.h
@@ -16,20 +16,24 @@ namespace hashmap
 
 		void Resize(size_t size);
 		bool Insert(const std::pair<K, V>& element);
-		bool Remove(const T& keyValue);
+		bool Remove(const K& keyValue);
 		std::pair<K, V>* FindNullable(const K& keyValue);
 		std::pair<K, V>& operator[](const K& keyValue);
+
+		bool SetLoadFactor(float loadFactor);
 
 
 
 	private:
-		size_t Hash(const K& keyValue) const;
+		size_t getHashValue(const K& keyValue, size_t hashTimes) const;
+		void reHash();
 
 
 		eState* mStateArray;
 		std::pair<K, V>* mData;
 		size_t mCapacity;
 		size_t mSize;
+		float mLoadFactor;
 
 
 	};
