2cee4e645fb839eeab50fba84b836b2864ebbda7
hsshimde <hsshimde@outlook.com>
2020-11-22 07:01:13 +0900

    added functions

--- VectorExperiments/VectorExperiments.h
+++ VectorExperiments/VectorExperiments.h
@@ -1,1 +1,14 @@
 #pragma once
+
+#include <vector>
+#include <unordered_map>
+
+namespace vectorExperiments
+{
+	int Sum(const std::vector<int>& v);
+	int Min(const std::vector<int>& v);
+	int Max(const std::vector<int>& v);
+	float Average(const std::vector<int>& v);
+	int NumberWithMaxOccurrence(const std::vector<int>& v);
+	void SortDescending(std::vector<int>& v);
+}
