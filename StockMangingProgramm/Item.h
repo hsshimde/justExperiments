0fffefd2cf097aa56c95e51e1d1b2b42de33f4ff
hsshimde <hsshimde@outlook.com>
2020-12-10 22:02:54 +0900

    Implementation in progress...

--- StockMangingProgramm/Item.h
+++ StockMangingProgramm/Item.h
@@ -3,6 +3,24 @@
 
 namespace manager
 {
-	
+	class Item 
+	{
+	public:
+		Item() = delete;
+		Item(const std::string& itemName, size_t itemNumber, size_t itemCount, size_t minimumStock);
+
+		bool Sell(size_t sellCount);
+		bool Buy(size_t buyCount);
+		
+
+		
+	private:
+		std::string mName;
+		size_t mItemNumber;
+		size_t mItemCount;
+		size_t mMinimumStock;
+		size_t mMaxStock;
+
+	};
 
 }
