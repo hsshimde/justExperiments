c016055c2b158bcdb4f30e8c469ea363a39a4462
hsshimde <hsshimde@outlook.com>
2020-12-25 23:57:24 +0900

    Implementation still in progress.....

--- Smart Data Structures Practice/SmartQueue.h
+++ Smart Data Structures Practice/SmartQueue.h
@@ -1,1 +1,19 @@
 #pragma once
+
+#include <queue>
+#include <limits>
+#include <cmath>
+
+namespace project
+{
+	template <typename T>
+	class SmartQueue
+	{
+	public:
+		SmartQueue() = default;
+		~SmartQueue() = default;
+
+	private:
+
+	};
+}
--- Smart Data Structures Practice/SmartStack.h
+++ Smart Data Structures Practice/SmartStack.h
@@ -1,7 +1,9 @@
 #pragma once
 #include <stack>
+#include <limits>
+#include <cmath>
 
-namespace smartstack
+namespace project
 {
 	template <typename T>
 	class SmartStack
@@ -13,11 +15,11 @@ namespace smartstack
 		SmartStack& operator=(const SmartStack& rhs) = default;
 
 		void Push(const T& element);
-		T& Peek() const;
+		T& Peek();
 		void Pop();
 
-		const T& GetMax() const;
-		const T& GetMin() const;
+		T GetMax() const;
+		T  GetMin() const;
 		double GetAverage() const;
 		double GetVariance() const;
 		double GetStandardDeviation() const;
@@ -27,12 +29,14 @@ namespace smartstack
 		std::stack<T> mMaxStack;
 		std::stack<T> mMinStack;
 		size_t mSize;
+		double mTotalSum;
+		double mTotalSquaredSum;
 		
 
 	};
 }
 
-namespace smartstack
+namespace project
 {
 	template <typename T>
 	void SmartStack<T>::Push(const T& element)
@@ -42,7 +46,93 @@ namespace smartstack
 			mMaxStack.push(element);
 			mMinStack.push(element);
 		}
+
+		else
+		{
+			if (element>=mMaxStack.top())
+			{
+				mMaxStack.push(element);
+			}
+			if (element <= mMinStack.top())
+			{
+				mMinStack.push(element);
+			}
+		}
 		mMainStack.push(element);
+		mTotalSum += element;
+		mTotalSquaredSum += static_cast<double>(element * element);
 		mSize++;
+	}
+
+
+	template <typename T>
+	T& SmartStack<T>::Peek() 
+	{
+		return mMainStack.top();
+	}
+
+	template <typename T>
+	void SmartStack<T>::Pop()
+	{
+		T& top = mMainStack.top();
+		if (top>=mMaxStack.top())
+		{
+			mMaxStack.pop();
+		}
+		if (top<=mMinStack.top())
+		{
+			mMinStack.pop();
+		}
+		mTotalSum -= top;
+		mTotalSquaredSum -= top * top;
+		mSize--;
+		return mMainStack.pop();
+	}
+
+	template <typename T>
+	T SmartStack<T>::GetMax() const
+	{
+		if (mSize<1)
+		{
+			return std::numeric_limits<T>::lowest();
+		}
+		else
+		{
+			return mMaxStack.top();
+		}
+	}
+
+	template <typename T>
+	T SmartStack<T>::GetMin() const
+	{
+		if (mSize<1)
+		{
+			return std::numeric_limits<T>::max();
+		}
+		else
+		{
+			return mMinStack.top();
+		}
+	}
+
+	template <typename T>
+	double SmartStack<T>::GetAverage() const
+	{
+		return mTotalSum / static_cast<double>(mSize);
+	}
+
+	template <typename T>
+	double SmartStack<T>::GetVariance() const
+	{
+		double average = GetAverage();
+		double variance = mTotalSquaredSum / static_cast<double>(mSize) - average * average;
+		return variance;
+	}
+
+	template <typename T>
+	double SmartStack<T>::GetStandardDeviation() const
+	{
+		double variance = GetVariance();
+		return std::sqrt(variance);
 	}
 }
