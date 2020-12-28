025eec4ebbe44c86bfee6d7fe7d1ce11a3a724e0
hsshimde <hsshimde@outlook.com>
2020-12-28 22:49:59 +0900

    QueueStack Implementation in progress....

--- Smart Data Structures Practice/QueueStack.h
+++ Smart Data Structures Practice/QueueStack.h
@@ -1,1 +1,33 @@
 #pragma once
+
+#include "SmartQueue.h"
+#include "SmartStack.h"
+
+
+namespace project
+{	
+	template <typename T>
+	class QueueStack
+	{
+	public:
+		QueueStack(size_t stackSize);
+		QueueStack(const QueueStack& rhs);
+		QueueStack(QueueStack&& rhs);
+		~QueueStack();
+		QueueStack& operator=(const QueueStack& rhs);
+		QueueStack& operator=(QueueStack&& rhs);
+
+		T GetMax();
+		T GetMin();
+		double GetAverage();
+
+	private:
+		std::queue<SmartStack<T>> mQueueStack;
+		size_t mMaxStackSize;
+		size_t mSize;
+		
+		
+		
+		
+	};
+}
