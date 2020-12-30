511f158f16ac017a40a8d48c2040a8c2cdfed698
hsshimde <hsshimde@outlook.com>
2020-12-30 19:17:26 +0900

    Implementation in progress..

--- Smart Data Structures Practice/QueueStack.h
+++ Smart Data Structures Practice/QueueStack.h
@@ -11,9 +11,10 @@ namespace project
 	{
 	public:
 		QueueStack(size_t stackSize);
-		QueueStack(const QueueStack& rhs);
+		QueueStack(QueueStack& rhs);
 		QueueStack(QueueStack&& rhs);
 		~QueueStack();
+
 		QueueStack& operator=(const QueueStack& rhs);
 		QueueStack& operator=(QueueStack&& rhs);
 
@@ -22,12 +23,76 @@ namespace project
 		double GetAverage();
 
 	private:
-		std::queue<SmartStack<T>> mQueueStack;
+		std::queue<SmartStack<T>*> mQueueStack;
 		size_t mMaxStackSize;
 		size_t mSize;
 		
-		
-		
-		
 	};
+}
+
+namespace project
+{
+	template <typename T>
+	QueueStack<T>::QueueStack(size_t stackSize)
+		: mQueueStack()
+		, mMaxStackSize(stackSize)
+		, mSize()	
+	{
+
+	}
+
+	template <typename T>
+	QueueStack<T>::QueueStack(QueueStack& rhs)
+		: mQueueStack()
+		, mMaxStackSize(rhs.mMaxStackSize)
+		, mSize(rhs.mSize)
+	{
+		for (size_t i = 0; i < mSize; i++)
+		{
+			mQueueStack.push(new SmartStack<T>(*rhs.mQueueStack.front()));
+			rhs.mQueueStack.push(rhs.mQueueStack.front());
+			rhs.mQueueStack.pop();
+		}
+	}
+
+	template <typename T>
+	QueueStack<T>::QueueStack(QueueStack&& rhs)
+		: mQueueStack()
+		, mMaxStackSize(rhs.mMaxStackSize)
+		, mSize(rhs.mSize)
+	{
+		for (size_t i = 0; i < mSize; i++)
+		{
+			mQueueStack.push(std::move(rhs.mQueueStack.front()));
+			rhs.mQueueStack.pop();
+		}
+	}
+
+	template <typename T>
+	QueueStack<T>::~QueueStack()
+	{
+		for (size_t i = 0; i < mSize; i++)
+		{
+			delete mQueueStack.front();
+			mQueueStack.pop();
+		}
+	}
+
+	template <typename T>
+	QueueStack<T>& QueueStack<T>::operator=(const QueueStack& rhs)
+	{
+		if (this != &rhs)
+		{
+			for (size_t i = 0; i < mSize; i++)
+			{
+				delete mQueueStack.front();
+				mQueueStack.pop();
+			}
+			mSize = rhs.mSize;
+			for (size_t i = 0; i < mSize; i++)
+			{
+				mQueueStack.push(new SmartStack<T>(*rhs.mQueueStack.front()));
+			}
+		}
+	}
 }
